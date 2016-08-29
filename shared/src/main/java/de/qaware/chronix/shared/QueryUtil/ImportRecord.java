4702f2392df1f1b0bb16c74bd3302e9864999a62
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-08-29 11:56:48 +0200

    added ImportRecord class that extends BenchmarkRecord to record measurements of import queries

---
+++ shared/src/main/java/de/qaware/chronix/shared/QueryUtil/ImportRecord.java
@@ -0,0 +1,34 @@
+package de.qaware.chronix.shared.QueryUtil;
+
+import de.qaware.chronix.database.TimeSeries;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by mcqueen666 on 29.08.16.
+ */
+@XmlRootElement
+public class ImportRecord extends BenchmarkRecord{
+
+    List<TimeSeries> timeSeriesList;
+
+
+    public ImportRecord(String queryID, String ipAddress, String portNumber, String tsdbName, List<TimeSeries> timeSeriesList) {
+        super(queryID, ipAddress, portNumber, tsdbName);
+        this.timeSeriesList = timeSeriesList;
+    }
+
+    public ImportRecord(BenchmarkRecord benchmarkRecord, List<TimeSeries> timeSeriesList) {
+        super(benchmarkRecord);
+        this.timeSeriesList = timeSeriesList;
+    }
+
+    public List<TimeSeries> getTimeSeriesList() {
+        return timeSeriesList;
+    }
+
+    public void setTimeSeriesList(List<TimeSeries> timeSeriesList) {
+        this.timeSeriesList = timeSeriesList;
+    }
+}
