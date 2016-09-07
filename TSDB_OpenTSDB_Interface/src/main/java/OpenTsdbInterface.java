fdc1d73903539c6135bf622b8283f9b1b6a83392
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-09-07 12:30:12 +0200

    OpenTSDB Interface: implemented performQuery()

--- TSDB_OpenTSDB_Interface/src/main/java/OpenTsdbInterface.java
+++ TSDB_OpenTSDB_Interface/src/main/java/OpenTsdbInterface.java
@@ -1,9 +1,9 @@
-import de.qaware.chronix.database.BenchmarkDataSource;
-import de.qaware.chronix.database.BenchmarkQuery;
-import de.qaware.chronix.database.TimeSeries;
-import de.qaware.chronix.database.TimeSeriesPoint;
-import jersey.repackaged.com.google.common.collect.Sets;
+import de.qaware.chronix.database.*;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
 /**
@@ -24,6 +24,7 @@ public class OpenTsdbInterface implements BenchmarkDataSource {
         if(!isSetup){
             try {
                 openTsdb = new OpenTsdb.Builder("http://" + ipAddress + ":" + portNumber).create();
+                //TODO wait until openTsdb is up and ready
                // openTsdb.setBatchSizeLimit(NUMBER_OF_POINTS_PER_BATCH);
                 // openTsdb cannot handle many points at once (o.0)
                 openTsdb.setBatchSizeLimit(OPENTSDB_NUMBER_OF_POINTS_PER_BATCH);
@@ -99,16 +100,81 @@ public class OpenTsdbInterface implements BenchmarkDataSource {
 
     @Override
     public Object getQueryObject(BenchmarkQuery benchmarkQuery) {
-        return new Object();
+        TimeSeriesMetaData timeSeriesMetaData = benchmarkQuery.getTimeSeriesMetaData();
+        QueryFunction function = benchmarkQuery.getFunction();
+        Map<String, String> tags = timeSeriesMetaData.getTagKey_tagValue();
+
+        String escapedMetricName = openTSDBEscapeValue(timeSeriesMetaData.getMetricName());
+
+        StringBuilder tagString = new StringBuilder();
+        tagString.append("{");
+        for(Map.Entry<String, String> tag : tags.entrySet()){
+            tagString.append(tag.getKey() + "=").append(openTSDBEscapeValue(tag.getValue())).append(",");
+        }
+        tagString.deleteCharAt(tagString.length()-1);
+        tagString.append("}");
+
+        String startDate = opentTSDBDate(Instant.ofEpochMilli(timeSeriesMetaData.getStart()));
+        String endDate = opentTSDBDate(Instant.ofEpochMilli(timeSeriesMetaData.getEnd()));
+
+        long timespan = Duration.between(Instant.ofEpochMilli(timeSeriesMetaData.getStart()), Instant.ofEpochMilli(timeSeriesMetaData.getEnd())).toMillis();
+        String aggregatedTimeSpan = timespan + "ms";
+
+
+        String defaultAggregatedMetric = "";
+        switch (function) {
+            case COUNT:     defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-count";
+                break;
+            case MEAN:      defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-avg";
+                break;
+            case SUM:       defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-sum";
+                break;
+            case MIN:       defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-min";
+                break;
+            case MAX:       defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-max";
+                break;
+            case STDDEV:    defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-dev";
+                break;
+            case PERCENTILE:
+                Float p = benchmarkQuery.getPercentile();
+                if (p != null) {
+                    if(p <= 0.5){
+                        defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-p50";
+                    } else if(p > 0.5 && p <= 0.75){
+                        defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-p75";
+                    } else if(p > 0.75 && p <= 0.9){
+                        defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-p90";
+                    } else if(p > 0.9 && p <= 0.95){
+                        defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-p95";
+                    } else {
+                        defaultAggregatedMetric = "sum:" + aggregatedTimeSpan + "-p99";
+                    }
+                }
+                break;
+            case QUERY_ONLY: defaultAggregatedMetric = "sum:" + aggregatedTimeSpan;
+        }
+
+        defaultAggregatedMetric = defaultAggregatedMetric + ":" + escapedMetricName + "{tags}";
+
+
+        return new OpenTsdbQuery(startDate, endDate, defaultAggregatedMetric, tagString.toString());
     }
 
     @Override
     public List<String> performQuery(BenchmarkQuery benchmarkQuery, Object queryObject) {
         List<String> queryResults = new LinkedList<>();
+        try{
+            OpenTsdbQuery query = ((OpenTsdbQuery) queryObject);
+            queryResults.add(openTsdb.query(query.getStartDate(),query.getEndDate(),query.getAggregatedMetric(),query.getTagString()));
+
+        } catch (Exception e){
+            queryResults.add("OpenTSDB error performing query: " + e.getLocalizedMessage());
+        }
+
         return queryResults;
     }
 
-    public static String openTSDBEscapeValue(String value) {
+    public String openTSDBEscapeValue(String value) {
         String escapedString = escape(value, ".").replaceAll("\\.\\.", ".").trim();
         escapedString = escapedString.replaceAll("%", "Percent").trim();
         escapedString = escapedString.replaceAll(":", "").trim();
@@ -124,7 +190,36 @@ public class OpenTsdbInterface implements BenchmarkDataSource {
         return escapedString;
     }
 
-    public static String escape(String metric, String replacement) {
+    public String escape(String metric, String replacement) {
         return metric.replaceAll("(\\s|\\.|:|=|,|/|\\\\|\\*|\\(|\\)|_|#)", replacement);
+    }
+
+    private String opentTSDBDate(Instant date) {
+        // "2014/12/27-12:48:20";
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(date, ZoneId.systemDefault());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(localDateTime.getYear())
+                .append("/")
+                .append(addDateSplit(localDateTime.getMonthValue()))
+                .append("/")
+                .append(addDateSplit(localDateTime.getDayOfMonth()))
+                .append("-")
+                .append(addDateSplit(localDateTime.getHour()))
+                .append(":")
+                .append(addDateSplit(localDateTime.getMinute()))
+                .append(":")
+                .append(addDateSplit(localDateTime.getSecond()));
+
+        return sb.toString();
+    }
+
+    private String addDateSplit(int value) {
+        if (value < 10) {
+            return "0" + value;
+        } else {
+            return "" + value;
+        }
     }
 }
