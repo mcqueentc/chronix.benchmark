19e866fdbc8fc27c356a3d48d3b3bf9220449f7c
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-09-23 00:09:14 +0200

    getValidRunCommand now has a flag to decide if a restart or a fresh start command should be generated.

--- shared/src/main/java/de/qaware/chronix/shared/dockerUtil/DockerRunOptions.java
+++ shared/src/main/java/de/qaware/chronix/shared/dockerUtil/DockerRunOptions.java
@@ -12,6 +12,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class DockerRunOptions {
 
     private String command;
+    private String restartCommand;
     private String containerName;
     private int hostPort;
     private int containerPort;
@@ -23,6 +24,7 @@ public class DockerRunOptions {
 
     public DockerRunOptions(String containerName, int hostPort, int containerPort, String additionalOptions){
         this.command = "docker run -d";
+        this.restartCommand = "docker restart";
         this.hostPort = hostPort;
         this.containerPort = containerPort;
         this.containerName = containerName;
@@ -50,6 +52,10 @@ public class DockerRunOptions {
         return this.additionalOptions;
     }
 
+    public String getRestartCommand() {
+        return restartCommand;
+    }
+
     // SET
     public void setCommand(String command){
         this.command = command;
@@ -71,14 +77,24 @@ public class DockerRunOptions {
         this.additionalOptions = additionalOptions;
     }
 
+    public void setRestartCommand(String restartCommand) {
+        this.restartCommand = restartCommand;
+    }
+
     @JsonIgnore
-    public String getValidRunCommand(){
+    public String getValidRunCommand(boolean shouldRestart){
         if(hostPort > 0 && containerPort > 0 && !containerName.isEmpty()){
-            if(isHarmlessString(command) && isHarmlessString(containerName) && isHarmlessString(additionalOptions)){
-                if(additionalOptions.isEmpty()){
-                    return command + " -p " + hostPort + ":" + containerPort + " " + containerName;
-                } else {
-                    return command + " -p " + hostPort + ":" + containerPort + " " + additionalOptions + " " + containerName;
+            if(shouldRestart){
+                if (isHarmlessString(restartCommand) && isHarmlessString(containerName)) {
+                    return restartCommand + "  " + containerName;
+                }
+            } else {
+                if (isHarmlessString(command) && isHarmlessString(containerName) && isHarmlessString(additionalOptions)) {
+                    if (additionalOptions.isEmpty()) {
+                        return command + " -p " + hostPort + ":" + containerPort + " " + containerName;
+                    } else {
+                        return command + " -p " + hostPort + ":" + containerPort + " " + additionalOptions + " " + containerName;
+                    }
                 }
             }
         }
