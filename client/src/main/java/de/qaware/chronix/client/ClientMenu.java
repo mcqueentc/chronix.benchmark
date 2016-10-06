d3caad7cc718831c38dce47951516ff200e2f155
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-10-06 17:43:13 +0200

    added class ClientMenu which is the entry point for the client.jar

---
+++ client/src/main/java/de/qaware/chronix/client/ClientMenu.java
@@ -0,0 +1,56 @@
+package de.qaware.chronix.client;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mcqueen666 on 06.10.16.
+ */
+public class ClientMenu {
+    public static void main(String[] args){
+        if(args.length == 0){
+            printUsage();
+            printFunctions();
+            return;
+        }
+
+        String function = args[0];
+        List<String> arguments = new ArrayList<String>(Arrays.asList(args));
+        arguments.remove(0);
+        args = arguments.toArray(new String[]{});
+
+        switch (function){
+            case "setup":
+                BenchmarkSetup.main(args);
+                break;
+
+            case "import":
+            case "convert":
+            case "benchmark":
+            case "build":
+            case "start":
+            case "stop":
+            case "clean":
+
+        }
+
+    }
+
+    private static void printUsage(){
+        System.out.println("Usage: java -jar chronixClient.jar [function]");
+        System.out.println("For more help per function call with function only.\n");
+    }
+
+    private static void printFunctions(){
+        System.out.println("Functions: \n");
+        System.out.println("setup:      configure client and server.");
+        System.out.println("import:     import json time series.");
+        System.out.println("convert:    convert .csv to json time series.");
+        System.out.println("benchmark:  run the benchmark (after you have imported something)");
+        System.out.println("build:      builds docker containers on the server");
+        System.out.println("start:      starts docker containers on the server");
+        System.out.println("stop:       stops docker containers on the server");
+        System.out.println("clean:      purge all data from TSDBs on the server");
+    }
+}
