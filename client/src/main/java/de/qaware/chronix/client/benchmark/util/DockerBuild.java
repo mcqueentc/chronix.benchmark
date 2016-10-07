b899f69f4beb314b3597489b52c0f63f2f22ebac
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-10-07 16:37:13 +0200

    added DockerBuild class to client and ClientMenu

---
+++ client/src/main/java/de/qaware/chronix/client/benchmark/util/DockerBuild.java
@@ -0,0 +1,56 @@
+package de.qaware.chronix.client.benchmark.util;
+
+import de.qaware.chronix.client.ClientMenu;
+import de.qaware.chronix.client.benchmark.configurator.Configurator;
+import de.qaware.chronix.shared.ServerConfig.ServerConfigAccessor;
+import de.qaware.chronix.shared.dockerUtil.DockerBuildOptions;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by mcqueen666 on 07.10.16.
+ */
+public class DockerBuild {
+    public static void main(String[] args){
+        if(args.length < 2){
+            printUsage();
+            return;
+        }
+
+        String server = args[0];
+        List<String> tsdbList;
+
+        Map<String, List<String>> serverTsdbMap = ClientMenu.getConfiguredServerAndTSDBs(args);
+        if(serverTsdbMap.isEmpty()){
+            System.err.println("Server: " + server + " was not configured!");
+            return;
+        }
+
+        tsdbList = serverTsdbMap.get(server);
+        if(tsdbList.isEmpty()){
+            System.err.println("No implemented tsdb interfaces found for your entries -> aborting");
+            return;
+        }
+
+        Configurator configurator = Configurator.getInstance();
+        ServerConfigAccessor serverConfigAccessor = ServerConfigAccessor.getInstance();
+        List<DockerBuildOptions> dockerBuildOptionsList = serverConfigAccessor.getServerConfigRecord(server).getTsdbBuildRecords();
+        String[] answers;
+        for(String tsdb : tsdbList){
+            for(DockerBuildOptions buildOption : dockerBuildOptionsList){
+                if(buildOption.getContainerName().equals(tsdb)){
+                    System.out.println("Building " + tsdb + " on server: " + server);
+                    answers = configurator.buildDockerContainer(server, buildOption);
+                    for(String s : answers){
+                        System.out.println("Server: " + s);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printUsage(){
+        System.out.println("Build usage: build [server] [tsdbName1] [tsdbName2] ...");
+    }
+}
