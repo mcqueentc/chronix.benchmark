f8d645fc8e698974491baedae801494521bc20db
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-10-10 22:05:26 +0200

    added method to initiate the time series analysis.

--- client/src/main/java/de/qaware/chronix/client/benchmark/resultpresenter/ResultPresenter.java
+++ client/src/main/java/de/qaware/chronix/client/benchmark/resultpresenter/ResultPresenter.java
@@ -1,7 +1,67 @@
 package de.qaware.chronix.client.benchmark.resultpresenter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.qaware.chronix.client.benchmark.configurator.Configurator;
+import de.qaware.chronix.shared.QueryUtil.JsonTimeSeriesHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by mcqueen666 on 20.06.16.
  */
 public class ResultPresenter {
+    private static ResultPresenter instance;
+
+    private final Logger logger = LoggerFactory.getLogger(ResultPresenter.class);
+    private final String statisticsDirectory = System.getProperty("user.home")
+            + File.separator
+            + "chronixBenchmark"
+            + File.separator
+            + "statistics";
+
+    private ResultPresenter(){
+        File dir = new File(statisticsDirectory);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+
+    }
+
+    public static ResultPresenter getInstance(){
+        if(instance == null){
+            instance = new ResultPresenter();
+        }
+        return instance;
+    }
+
+    public String getStatisticsDirectory() {
+        return statisticsDirectory;
+    }
+
+    public void analyzeTimeSeries(){
+        File directory = new File(JsonTimeSeriesHandler.getInstance().getTimeSeriesJsonRecordDirectoryPath());
+        File[] files = directory.listFiles();
+        List<File> measurements = new LinkedList<>();
+        for(File dir : files){
+            if(dir.isDirectory()){
+                measurements.add(dir);
+            }
+        }
+
+        TimeSeriesAnalyzer timeSeriesAnalyzer = new TimeSeriesAnalyzer();
+        TimeSeriesStatistics statistics = timeSeriesAnalyzer.analyzeJsonTimeSeries(measurements);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            final String statisticsJsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(statistics);
+            System.out.println(statisticsJsonString);
+
+        } catch (Exception e) {
+           logger.error("Error generating json string: " + e.getLocalizedMessage());
+        }
+    }
 }
