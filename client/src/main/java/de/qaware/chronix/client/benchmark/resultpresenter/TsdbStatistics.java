923e1ae0e973572347a7006934feabece243f1ea
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-10-16 20:35:04 +0200

    added the infos about the query benchmark to the records

--- client/src/main/java/de/qaware/chronix/client/benchmark/resultpresenter/TsdbStatistics.java
+++ client/src/main/java/de/qaware/chronix/client/benchmark/resultpresenter/TsdbStatistics.java
@@ -10,6 +10,12 @@ import java.util.List;
 public class TsdbStatistics {
 
     private String tsdbName;
+    private int numberOfTimeSeriesPerQuery;
+    private int numberOfTimeSeriesPer_QUERY_ONLY_Function;
+    private int numberOfQueriesPerQueryFunction;
+    private int totalNumberOfPerformedQueries;
+    private int totalNumberOfPerformedImports;
+
     private List<QueryFunctionStatistics> queryFunctionStatisticsList;
 
     public TsdbStatistics(String tsdbName, List<QueryFunctionStatistics> queryFunctionStatisticsList) {
@@ -34,5 +40,45 @@ public class TsdbStatistics {
 
     public void setQueryFunctionStatisticsList(List<QueryFunctionStatistics> queryFunctionStatisticsList) {
         this.queryFunctionStatisticsList = queryFunctionStatisticsList;
+    }
+
+    public int getNumberOfTimeSeriesPerQuery() {
+        return numberOfTimeSeriesPerQuery;
+    }
+
+    public void setNumberOfTimeSeriesPerQuery(int numberOfTimeSeriesPerQuery) {
+        this.numberOfTimeSeriesPerQuery = numberOfTimeSeriesPerQuery;
+    }
+
+    public int getNumberOfQueriesPerQueryFunction() {
+        return numberOfQueriesPerQueryFunction;
+    }
+
+    public void setNumberOfQueriesPerQueryFunction(int numberOfQueriesPerQueryFunction) {
+        this.numberOfQueriesPerQueryFunction = numberOfQueriesPerQueryFunction;
+    }
+
+    public int getTotalNumberOfPerformedQueries() {
+        return totalNumberOfPerformedQueries;
+    }
+
+    public void setTotalNumberOfPerformedQueries(int totalNumberOfPerformedQueries) {
+        this.totalNumberOfPerformedQueries = totalNumberOfPerformedQueries;
+    }
+
+    public int getTotalNumberOfPerformedImports() {
+        return totalNumberOfPerformedImports;
+    }
+
+    public void setTotalNumberOfPerformedImports(int totalNumberOfPerformedImports) {
+        this.totalNumberOfPerformedImports = totalNumberOfPerformedImports;
+    }
+
+    public int getNumberOfTimeSeriesPer_QUERY_ONLY_Function() {
+        return numberOfTimeSeriesPer_QUERY_ONLY_Function;
+    }
+
+    public void setNumberOfTimeSeriesPer_QUERY_ONLY_Function(int numberOfTimeSeriesPer_QUERY_ONLY_Function) {
+        this.numberOfTimeSeriesPer_QUERY_ONLY_Function = numberOfTimeSeriesPer_QUERY_ONLY_Function;
     }
 }
