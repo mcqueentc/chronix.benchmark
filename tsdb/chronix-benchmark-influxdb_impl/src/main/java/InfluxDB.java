46e025199bd6f544aa4a079076899a5c218b98cf
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-11-02 18:05:46 +0100

    moved the impls to tsdb folder

--- tsdb/chronix-benchmark-influxdb_impl/src/main/java/InfluxDB.java
+++ tsdb/chronix-benchmark-influxdb_impl/src/main/java/InfluxDB.java
@@ -4,7 +4,6 @@ import org.influxdb.dto.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
